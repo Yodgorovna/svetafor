86b6a254ace4a65553f795ab0a1e93d654b015b2
Yodgorovna <lobargoziyeva9@gmail.com>
2024-05-27 00:25:58 +0500

    codesss

--- main.js
+++ main.js
@@ -8,18 +8,38 @@ const greenElement = document.querySelector(
   "header .container .svetafor .green"
 );
 
-setTimeout(() => {
-  if ((redElement.style.backgroundColor = "gray")) {
-    redElement.style.backgroundColor = "red";
-  }
-  redElement.style.backgroundColor = "red";
-  if ((redElement.style.backgroundColor = "red")) {
-    redElement.style.backgroundColor = "blue";
-    yellowElement.style.backgroundColor = "yellow";
-  }
-  redElement.style.backgroundColor = "blue";
-  if ((yellowElement.style.backgroundColor = "yellow")) {
-    yellowElement.style.backgroundColor = "blue";
-    greenElement.style.backgroundColor = "green";
-  }
-}, 20000);
+function startInterval1() {
+  let id1 = setInterval(() => {
+    if ((redElement.style.backgroundColor = "gray")) {
+      greenElement.style.backgroundColor = "gray";
+      redElement.style.backgroundColor = "red";
+
+      clearInterval(id1);
+      startInterval2();
+    }
+  }, 5000);
+}
+
+function startInterval2() {
+  let id2 = setInterval(() => {
+    if ((redElement.style.backgroundColor = "red")) {
+      yellowElement.style.backgroundColor = "yellow";
+      clearInterval(id2);
+      startInterval3();
+    }
+  }, 2000);
+}
+
+function startInterval3() {
+  let id3 = setInterval(() => {
+    if ((yellowElement.style.backgroundColor = "yellow")) {
+      redElement.style.backgroundColor = "gray";
+      yellowElement.style.backgroundColor = "gray";
+      greenElement.style.backgroundColor = "green";
+      clearInterval(id3);
+      startInterval1();
+    }
+  }, 5000);
+}
+
+startInterval1();
